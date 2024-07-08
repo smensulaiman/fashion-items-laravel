522978da0d157873975254bfa95c29d3c54b0a2d
MD SULAIMAN <smensulaiman007@gmail.com>
2024-07-08 16:57:18 +0900

    Middleware added for multiple vendor

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,6 +28,12 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
+        if($request->user()->role == 'admin'){
+            return redirect()->intended(route('admin.dashboard'));
+        }else if ($request->user()->role == 'vendor'){
+            return redirect()->intended(route('vendor.dashboard'));
+        }
+
         return redirect()->intended(route('dashboard', absolute: false));
     }
 
