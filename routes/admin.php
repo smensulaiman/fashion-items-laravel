c829810860dd0abafe57188bcd5b74fe8b10527f
MD SULAIMAN <smensulaiman007@gmail.com>
2024-07-09 11:47:51 +0900

    Custom Route Brush Up

--- routes/admin.php
+++ routes/admin.php
@@ -7,5 +7,4 @@ use App\Http\Controllers\Backend\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('admin/dashboard', array(AdminController::class, 'dashboard'))
-    ->middleware(['auth', 'role:admin'])
     ->name('admin.dashboard');
--- routes/vendor.php
+++ routes/vendor.php
@@ -7,5 +7,4 @@ use App\Http\Controllers\Backend\VendorController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('vendor/dashboard', array(VendorController::class, 'dashboard'))
-    ->middleware(['auth', 'role:vendor'])
     ->name('vendor.dashboard');
