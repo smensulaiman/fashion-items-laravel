904132a51999f36e05eb9ec2693c47b4a5d86c21
MD SULAIMAN <smensulaiman007@gmail.com>
2024-07-17 17:30:38 +0900

    frontend signup add

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -74,57 +74,6 @@
                                 </div>
                             </div>
 
-                            <form method="POST" action="{{ route('register') }}">
-                                @csrf
-
-                                <!-- Name -->
-                                <div>
-                                    <x-input-label for="name" :value="__('Name')" />
-                                    <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-                                    <x-input-error :messages="$errors->get('name')" class="mt-2" />
-                                </div>
-
-                                <!-- Email Address -->
-                                <div class="mt-4">
-                                    <x-input-label for="email" :value="__('Email')" />
-                                    <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-                                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
-                                </div>
-
-                                <!-- Password -->
-                                <div class="mt-4">
-                                    <x-input-label for="password" :value="__('Password')" />
-
-                                    <x-text-input id="password" class="block mt-1 w-full"
-                                                  type="password"
-                                                  name="password"
-                                                  required autocomplete="new-password" />
-
-                                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
-                                </div>
-
-                                <!-- Confirm Password -->
-                                <div class="mt-4">
-                                    <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                                    <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                                  type="password"
-                                                  name="password_confirmation" required autocomplete="new-password" />
-
-                                    <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-                                </div>
-
-                                <div class="flex items-center justify-end mt-4">
-                                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                                        {{ __('Already registered?') }}
-                                    </a>
-
-                                    <x-primary-button class="ms-4">
-                                        {{ __('Register') }}
-                                    </x-primary-button>
-                                </div>
-                            </form>
-
                             <div class="tab-pane fade" id="pills-profiles" role="tabpanel"
                                  aria-labelledby="pills-profile-tab2">
                                 <div class="wsus__login">
@@ -151,8 +100,7 @@
                                             <div class="form-check form-switch">
                                                 <input class="form-check-input" type="checkbox"
                                                        id="flexSwitchCheckDefault03">
-                                                <label class="form-check-label" for="flexSwitchCheckDefault03">I consent
-                                                    to the privacy policy</label>
+                                                <label class="form-check-label" for="flexSwitchCheckDefault03">I consentto the privacy policy</label>
                                             </div>
                                         </div>
                                         <button class="common_btn" type="submit">signup</button>
